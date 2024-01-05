776e581a3b4486b234dab7fcff60a1ff0ec5fd65
matheushenriquecsb <matheus.mhg2@gmail.com>
2024-01-05 16:28:13 -0300

    feat: setup prisma database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { PrismaService } from './config/database/prisma.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TasksModule],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
